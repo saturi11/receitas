4a3a06f175b5f60de5d570755c9f16a6d5fd2df5
Gabriel Saturi <gabrielsaturi11@gmail.com>
2026-03-26 11:37:10 -0300

    Feat: adding

---
+++ src/Shared/MyRecipeBook.Communication/Responses/ResponseErrorJson.cs
@@ -0,0 +1,16 @@
+﻿namespace MyRecipeBook.Communication.Responses
+{
+    public class ResponseErrorJson
+    {
+        public IList<string> Erros { get; set; }
+        public ResponseErrorJson(IList<string> erros) => Erros = erros;
+
+
+        public ResponseErrorJson(string error)
+        {
+            Erros = [error];
+
+        }
+
+    }
+}
